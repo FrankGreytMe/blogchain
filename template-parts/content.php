c754e548a275c158a97dfd234db04bba8c02dca3
Freelancingonupwork <56026691+Freelancingonupwork@users.noreply.github.com>
2025-07-02 12:31:08 +0000

    Added Questionnaire Capsule on blog detail page.

--- template-parts/content.php
+++ template-parts/content.php
@@ -53,7 +53,7 @@ $classes       = get_theme_mod( 'archive_outlined', false ) ? 'mdc-card--outline
                     ?>
                 "
                 >
-                    <?php echo get_simple_local_avatar( get_the_author_meta( 'ID' ), 18 ); ?>
+                    <?php echo get_avatar( get_the_author_meta( 'ID' ), 18 ); ?>
                     <?php the_author(); ?>
                 </a>
             <?php endif; ?>
